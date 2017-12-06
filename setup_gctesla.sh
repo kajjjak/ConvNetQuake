31420efde1467b41ff7581fc913dbcfd066cc2ea
kajjjak <mail@kjartanjonsson.com>
2017-12-06 08:36:54 +0000

    Update setup_gctesla.sh

--- setup_gctesla.sh
+++ setup_gctesla.sh
@@ -1,9 +1,8 @@
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-sudo apt-get update
-sudo apt-get install cuda
-
+sudo apt-get -y update
+sudo apt-get -y install cuda
 echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
 echo 'export PATH=$PATH:$CUDA_HOME/bin' >> ~/.bashrc
 echo 'export LD_LIBRARY_PATH=$CUDA_HOME/lib64' >> ~/.bashrc

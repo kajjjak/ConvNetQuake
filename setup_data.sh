fa454d850ea902a347f4b3dec2e80f67e4d0f81c
kajjjak <mail@kjartanjonsson.com>
2017-12-06 10:19:17 +0000

    Create setup_data.sh

---
+++ setup_data.sh
@@ -0,0 +1,8 @@
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install gcsfuse
+gcloud auth application-default login
+mkdir ConvNetQuake/data
+gcsfuse convnetquake ConvNetQuake/data

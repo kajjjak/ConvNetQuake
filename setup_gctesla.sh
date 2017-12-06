d3442e616dcc2d0f22c45d908afcaaf9d6196fbb
kajjjak <mail@kjartanjonsson.com>
2017-12-06 09:00:09 +0000

    Testing another way
    https://hackernoon.com/launch-a-gpu-backed-google-compute-engine-instance-and-setup-tensorflow-keras-and-jupyter-902369ed5272

--- setup_gctesla.sh
+++ setup_gctesla.sh
@@ -1,18 +1,24 @@
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-sudo apt-get -y update
-sudo apt-get -y install cuda
-echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
-echo 'export PATH=$PATH:$CUDA_HOME/bin' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=$CUDA_HOME/lib64' >> ~/.bashrc
+curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo apt-get update
+sudo apt-get install -y cuda-8-0
+cat <<EOF >> ~/.bashrc
+export CUDA_HOME=/usr/local/cuda-8.0
+export LD_LIBRARY_PATH=\${CUDA_HOME}/lib64
+export PATH=\${CUDA_HOME}/bin:\${PATH}
+EOF
 source ~/.bashrc
+cuda-install-samples-8.0.sh ~
+cd ~/NVIDIA_CUDA-8.0_Samples/1_Utilities/deviceQuery
+make
+./deviceQuery
+cd ~
 wget https://storage.googleapis.com/convnetquake/cudnn-8.0-linux-x64-v6.0.tgz
-tar xzvf cudnn-8.0-linux-x64-v6.0.tgz
-sudo cp cuda/lib64/* /usr/local/cuda/lib64/
-sudo cp cuda/include/cudnn.h /usr/local/cuda/include/
-rm -rf ~/cuda
-rm cudnn-8.0-linux-x64-v6.0.tgz
+tar xvzf cudnn-8.0-linux-x64-v5.1.tgz
+sudo cp -P cuda/include/cudnn.h $CUDA_HOME/include
+sudo cp -P cuda/lib64/libcudnn $CUDA_HOME/lib64
+sudo chmod u+w $CUDA_HOME/include/cudnn.h
+sudo chmod a+r $CUDA_HOME/lib64/libcudnn
 export LC_ALL=C
 sudo apt-get install python-dev python-pip libcupti-dev
 sudo pip install tensorflow-gpu

80d360ccaa5b83ee8efcd520ee4c976af1e53188
kajjjak <mail@kjartanjonsson.com>
2017-12-06 10:49:10 +0000

    Update prep_data.sh

--- prep_data.sh
+++ prep_data.sh
@@ -7,3 +7,5 @@ cd ..
 cd ..
 sudo ./bin/preprocess/cluster_events --src data/catalogs/OK_2014-2015-2016.csv --dst data/6_clusters --n_components 6 --model KMeans
 sudo ./bin/preprocess/create_dataset_events.py --stream_dir data/streams --catalog data/6_clusters/catalog_with_cluster_ids.csv --output_dir data/6_clusters/events --save_mseed True --plot True
+sudo ./bin/preprocess/data_augmentation.py --tfrecords data/6_clusters/events --output data/6_clusters/augmented_data/augmented_stetch_std1-2.tfrecords --std_factor 1.2
+sudo ./bin/preprocess/create_dataset_noise.py --stream_path data/streams/GSOK029_8-2014.mseed --catalog data/catalogs/Benz_catalog.csv --output_dir data/noise_OK029/noise_august

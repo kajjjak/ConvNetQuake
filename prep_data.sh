5a298961f3adf0ccbb4ac1c3dd00f5bf1522e4c5
kajjjak <mail@kjartanjonsson.com>
2017-12-06 10:07:29 +0000

    Create prep_data.sh

---
+++ prep_data.sh
@@ -0,0 +1,5 @@
+mkdir data
+mkdir data/catalogs
+cd data/catalogs
+wget https://www.dropbox.com/sh/3p9rmi1bcpvnk5k/AAC_1lEoII2y64j92dKjLBBSa/catalogs/Benz_catalog.csv
+wget https://www.dropbox.com/sh/3p9rmi1bcpvnk5k/AACU5NxcaiBfD8cqOzK7pctWa/catalogs/OK_2014-2015-2016.csv

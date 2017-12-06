da5142acd802374ab2477ab6567f7d3846426766
kajjjak <mail@kjartanjonsson.com>
2017-12-06 09:27:30 +0000

    cudnn-8.0-linux-x64-v6.0.tgz

--- setup_gctesla.sh
+++ setup_gctesla.sh
@@ -14,7 +14,7 @@ make
 ./deviceQuery
 cd ~
 wget https://storage.googleapis.com/convnetquake/cudnn-8.0-linux-x64-v6.0.tgz
-tar xvzf cudnn-8.0-linux-x64-v5.1.tgz
+tar xvzf cudnn-8.0-linux-x64-v6.0.tgz
 sudo cp -P cuda/include/cudnn.h $CUDA_HOME/include
 sudo cp -P cuda/lib64/libcudnn $CUDA_HOME/lib64
 sudo chmod u+w $CUDA_HOME/include/cudnn.h

decbfd5de7e8b44cb512d22dd0518993cc9b291b
kajjjak <mail@kjartanjonsson.com>
2017-12-06 15:44:29 +0000

    Update create_swap.sh

--- create_swap.sh
+++ create_swap.sh
@@ -1,6 +1,7 @@
+# https://www.digitalocean.com/community/tutorials/how-to-configure-virtual-memory-swap-file-on-a-vps
 cd /var
 touch swap.img
 chmod 600 swap.img
-dd if=/dev/zero of=/var/swap.img bs=1024k count=1000
+dd if=/dev/zero of=/var/swap.img bs=8000k count=1000
 mkswap /var/swap.img
 swapon /var/swap.img

e364158b06be62f556f61a84e5496ee13e47198c
kajjjak <mail@kjartanjonsson.com>
2017-12-11 22:05:03 +0000

    Update setup_gcteslapaths.sh

--- setup_gcteslapaths.sh
+++ setup_gcteslapaths.sh
@@ -1,6 +1,9 @@
 #cat <<EOF >> ~/.bashrc
 export CUDA_HOME=/usr/local/cuda-8.0
+^D
 export LD_LIBRARY_PATH=\${CUDA_HOME}/lib64
+^D
 export PATH=\${CUDA_HOME}/bin:\${PATH}
+^D
 #EOF
 #source ~/.bashrc

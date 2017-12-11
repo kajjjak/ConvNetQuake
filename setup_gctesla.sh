a46a558ee3e5842fdc31ee4d74a28f10250d968f
kajjjak <mail@kjartanjonsson.com>
2017-12-11 23:26:19 +0000

    Update setup_gctesla.sh

--- setup_gctesla.sh
+++ setup_gctesla.sh
@@ -2,14 +2,17 @@ curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_
 sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo apt-get -y update
 sudo apt-get -y install cuda-8-0
-sudo ./setup_gcteslapaths.sh
-wget https://storage.googleapis.com/convnetquake/cudnn-8.0-linux-x64-v6.0.tgz
-tar xvzf cudnn-8.0-linux-x64-v6.0.tgz
-cuda-install-samples-8.0.sh ~
-cd ~/NVIDIA_CUDA-8.0_Samples/1_Utilities/deviceQuery
-make
-./deviceQuery
-cd ~
+CUDA_HOME=/usr/local/cuda-8.0
+LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64
+#PATH=\${CUDA_HOME}/bin:\${PATH}
+#sudo ./setup_gcteslapaths.sh
+#wget https://storage.googleapis.com/convnetquake/cudnn-8.0-linux-x64-v6.0.tgz
+#tar xvzf cudnn-8.0-linux-x64-v6.0.tgz
+#cuda-install-samples-8.0.sh ~
+#cd ~/NVIDIA_CUDA-8.0_Samples/1_Utilities/deviceQuery
+#make
+#./deviceQuery
+#cd ~
 sudo cp -P cuda/include/cudnn.h $CUDA_HOME/include
 sudo cp -P cuda/lib64/libcudnn* $CUDA_HOME/lib64
 sudo chmod u+w $CUDA_HOME/include/cudnn.h

6523de3f543b383694765e658f768d4d0ff8c967
kajjjak <mail@kjartanjonsson.com>
2017-12-11 14:32:21 +0000

    Update setup_gctesla.sh

--- setup_gctesla.sh
+++ setup_gctesla.sh
@@ -3,17 +3,7 @@ curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_
 sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo apt-get -y update
 sudo apt-get -y install cuda-8-0
-cat <<EOF >> ~/.bashrc
-export CUDA_HOME=/usr/local/cuda-8.0
-export LD_LIBRARY_PATH=\${CUDA_HOME}/lib64
-export PATH=\${CUDA_HOME}/bin:\${PATH}
-EOF
-source ~/.bashrc
-#cuda-install-samples-8.0.sh ~
-#cd ~/NVIDIA_CUDA-8.0_Samples/1_Utilities/deviceQuery
-#make
-#./deviceQuery
-#cd ~
+./setup_gcteslapaths.sh
 wget https://storage.googleapis.com/convnetquake/cudnn-8.0-linux-x64-v6.0.tgz
 tar xvzf cudnn-8.0-linux-x64-v6.0.tgz
 sudo cp -P cuda/include/cudnn.h $CUDA_HOME/include
@@ -23,3 +13,8 @@ sudo chmod a+r $CUDA_HOME/lib64/libcudnn*
 export LC_ALL=C
 sudo apt-get -y install python-dev python-pip libcupti-dev
 sudo pip install tensorflow-gpu
+cuda-install-samples-8.0.sh ~
+cd ~/NVIDIA_CUDA-8.0_Samples/1_Utilities/deviceQuery
+make
+./deviceQuery
+cd ~

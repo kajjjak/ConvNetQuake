06aa6543eaf97ee3f500edc646f41efa4eeed8af
Your Name <you@example.com>
2017-12-11 23:03:50 +0000

    create swap sudo

--- create_swap.sh
+++ create_swap.sh
@@ -1,7 +1,7 @@
 # https://www.digitalocean.com/community/tutorials/how-to-configure-virtual-memory-swap-file-on-a-vps
 cd /var
-touch swap.img
-chmod 600 swap.img
-dd if=/dev/zero of=/var/swap.img bs=8000k count=1000
-mkswap /var/swap.img
-swapon /var/swap.img
+sudo touch swap.img
+sudo chmod 600 swap.img
+sudo dd if=/dev/zero of=/var/swap.img bs=8000k count=1000
+sudo mkswap /var/swap.img
+sudo swapon /var/swap.img

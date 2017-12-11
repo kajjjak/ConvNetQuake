de20170496d357c297b5ac3fd864bf2cd63cdf36
kajjjak <mail@kjartanjonsson.com>
2017-12-11 15:13:20 +0000

    Update prep_data.sh

--- prep_data.sh
+++ prep_data.sh
@@ -1,11 +1,14 @@
-gcloud compute scp --recurse /media/kjartan/DATADRIVE11/ConNetQuake/data/*  instance-convnetquake:/home/kjartan/ConvNetQuake/data --zone us-east1-c
+# gcloud compute scp --recurse /media/kjartan/DATADRIVE11/ConNetQuake/data/*  instance-convnetquake:/home/kjartan/ConvNetQuake/data --zone us-east1-c
+cd ~/ConvNetQuake
 export PYTHONPATH=$PWD
-sudo ./bin/preprocess/cluster_events --src data/catalogs/OK_2014-2015-2016.csv --dst data/6_clusters --n_components 6 --model KMeans
-sudo ./bin/preprocess/create_dataset_events.py --stream_dir data/streams --catalog data/6_clusters/catalog_with_cluster_ids.csv --output_dir data/6_clusters/events --save_mseed True --plot True
-sudo ./bin/preprocess/data_augmentation.py --tfrecords data/6_clusters/events --output data/6_clusters/augmented_data/augmented_stetch_std1-2.tfrecords --std_factor 1.2
-sudo ./bin/preprocess/create_dataset_noise.py --stream_path data/streams/GSOK029_8-2014.mseed --catalog data/catalogs/Benz_catalog.csv --output_dir data/noise_OK029/noise_august
-sudo tensorboard --port 80  --logdir output/convnetquake/ConvNetQuake &
-
+./bin/preprocess/cluster_events --src data/catalogs/OK_2014-2015-2016.csv --dst data/6_clusters --n_components 6 --model KMeans
+./bin/preprocess/create_dataset_events.py --stream_dir data/streams --catalog data/6_clusters/catalog_with_cluster_ids.csv --output_dir data/6_clusters/events --save_mseed True --plot True
+./bin/preprocess/data_augmentation.py --tfrecords data/6_clusters/events --output data/6_clusters/augmented_data/augmented_stetch_std1-2.tfrecords --std_factor 1.2
+./bin/preprocess/create_dataset_noise.py --stream_path data/streams/GSOK029_8-2014.mseed --catalog data/catalogs/Benz_catalog.csv --output_dir data/noise_OK029/noise_august
+mkdir ~/ConvNetQuake/data/6_clusters/train/
+mkdir ~/ConvNetQuake/data/6_clusters/train/negative
+mkdir ~/ConvNetQuake/data/6_clusters/train/positive
+cd ~/ConvNetQuake/data/6_clusters/events
 cp GS0K029_10-2015.tfrecords  ../train/negative
 cp GSOK027_2-2014.tfrecords ../train/negative
 cp GSOK029_1-2016.tfrecords ../train/negative
@@ -52,9 +55,9 @@ cp GSOK029_5-2014.tfrecords ../train/positive
 cp GSOK027_12-2014.tfrecords ../train/positive
 cp GSOK029_1-2015.tfrecords ../train/positive
 cp GSOK029_5-2016.tfrecords ../train/positive
-cd ..
-cd ..
-mkdir output
-output/convnetquake
+cd ~/ConvNetQuake
+mkdir ~/ConvNetQuake/output
+mkdir ~/ConvNetQuake/convnetquake
+sudo tensorboard --port 80  --logdir ~/ConvNetQuake/output/convnetquake/ConvNetQuake &
 ./bin/train --dataset data/6_clusters/train --checkpoint_dir output/convnetquake --n_clusters 6
 

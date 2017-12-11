4893f5ea0a140cb2269b6ceee8efa63970628527
kajjjak <mail@kjartanjonsson.com>
2017-12-11 06:40:37 +0000

    Update setup_gctesla.sh

--- setup_gctesla.sh
+++ setup_gctesla.sh
@@ -1,3 +1,4 @@
+sudo su
 curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo apt-get -y update

d9fb34fdd26635ca67c985014b4122567d540519
kajjjak <mail@kjartanjonsson.com>
2017-12-12 06:04:28 +0000

    Create setup_dependencytesla.sh

---
+++ setup_dependencytesla.sh
@@ -0,0 +1,25 @@
+sudo apt-get -y install python-pip
+sudo apt-get -y install python-tk
+pip install numpy
+pip install Cython
+pip install obspy
+pip install pandas
+pip install python-gflags
+pip install -U scikit-learn
+pip install EQcorrscan==0.1.6
+pip install librosa==0.4.3
+pip install matplotlib==1.5.3
+pip install numpy==1.11.2
+pip install obspy==1.0.2
+pip install pytest==2.8.7
+pip install -e git+https://github.com/gem/oq-hazardlib.git@589fa31ddca3697e6b167082136dc0174a77bc16#egg=openquake.hazardlib
+pip install pandas==0.19.0
+pip install protobuf==3.0.0
+pip install scikit-image==0.12.3
+pip install scikit-learn==0.18
+pip install scipy==0.18.1
+pip install seaborn==0.7.1
+pip install setproctitle==1.1.10
+pip install tensorflow-gpu==0.12.1
+pip install terminado==0.6
+pip install tqdm==4.9.0

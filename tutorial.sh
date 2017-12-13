982bacb9b0e0c4bb79e00800c6b6fa4fcb3007a6
kajjjak <mail@kjartanjonsson.com>
2017-12-13 12:26:57 +0000

    Update tutorial.sh

--- tutorial.sh
+++ tutorial.sh
@@ -1,12 +1,8 @@
+cd ~/ConvNetQuake
+export PYTHONPATH=$PWD
 ./bin/preprocess/cluster_events --src data/catalogs/OK_2014-2015-2016.csv --dst data/6_clusters --n_components 6 --model KMeans
 
-
 ./bin/preprocess/create_dataset_events.py --stream_dir data/streams --catalog data/6_clusters/catalog_with_cluster_ids.csv --output_dir data/6_clusters/events --save_mseed True --plot True
-
 ./bin/preprocess/data_augmentation.py --tfrecords data/6_clusters/events --output data/6_clusters/augmented_data/augmented_stetch_std1-2.tfrecords --std_factor 1.2
-
-
 ./bin/preprocess/create_dataset_noise.py --stream_path data/streams/GSOK027_2-2014.mseed --catalog data/catalogs/Benz_catalog.csv --output_dir data/noise_OK029/noise_august
-
-
 ./bin/train --dataset data/6_clusters/train --checkpoint_dir output/convnetquake --n_clusters 6
